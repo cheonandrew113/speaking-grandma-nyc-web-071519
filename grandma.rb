0f39d02ef026db15b9a09f7258e40d648972d884
Andnrew Cheon <cheonandrew113@gmail.com>
2019-07-15 20:57:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(argument)
-  if argument.include?("!")
+  if argument.include?("!") && argument == argument.upcase
     return "I LOVE YOU TOO PUMPKIN!"
   elsif argument == argument.upcase
     return "NO, NOT SINCE 1938!"
